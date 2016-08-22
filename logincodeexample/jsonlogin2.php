06c417471537611f297109d393eed758fdebd15e
Louise <wddtechnologies@gmail.com>
2016-08-22 12:03:42 -0400

    changes to jsonlogin2
    fixed coding

--- logincodeexample/jsonlogin2.php
+++ logincodeexample/jsonlogin2.php
@@ -1,4 +1,3 @@
-<?php
 /*----------------------*/
 /* Author: Louise Rennick */
 /* Website: DomainName.com */
@@ -6,58 +5,59 @@
 /*-------------------------*/
 
 header('Content-type: application/json');
-		if($_POST) {
-		$username   = $_POST['username'];
-		$password   = $_POST['password'];
-		$c_password = $_POST['c_password'];
+if($_POST) {
+$username   = $_POST['username'];
+$password   = $_POST['password'];
 
-	if($_POST['username']) {
-	if ( $password == $c_password ) {
+if($username && $password) {
 
-	$db_name     = 'DB_NAME';
-	$db_user     = 'DB_USER';
-	$db_password = 'DB_PASSWORD';
-	$server_url  = 'localhost';
+$db_name     = 'DB_NAME';
+$db_user     = 'DB_USER';
+$db_password = 'DB_PASS';
+$server_url  = 'localhost';
 
-	$mysqli = new mysqli('localhost', $db_user, $db_password, $db_name);
+$mysqli = new mysqli('localhost', $db_user, $db_password, $db_name);
 
 /* check connection */
-	if (mysqli_connect_errno()) {
-		error_log("Connect failed: " . mysqli_connect_error());
-		echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
-	} else {
-		$stmt = $mysqli->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-		$password = md5($password);
-		$stmt->bind_param('ss', $username, $password);
+if (mysqli_connect_errno()) {
+error_log("Connect failed: " . mysqli_connect_error());
+echo '{"success":0,"error_message":"' . mysqli_connect_error() . '"}';
+} else {
+if ($stmt = $mysqli->prepare("SELECT username FROM users WHERE username = ? and password = ?")) {
 
-/* execute prepared statement */
-	$stmt->execute();
+$password = md5($password);
 
-	if ($stmt->error) {error_log("Error: " . $stmt->error); }
+/* bind parameters for markers */
+$stmt->bind_param("ss", $username, $password);
 
-	$success = $stmt->affected_rows;
+/* execute query */
+$stmt->execute();
 
-/* close statement and connection */
-	$stmt->close();
+/* bind result variables */
+$stmt->bind_result($id);
+
+/* fetch value */
+$stmt->fetch();
+
+/* close statement */
+$stmt->close();
+}
 
 /* close connection */
-	$mysqli->close();
-	error_log("Success: $success");
+$mysqli->close();
 
-		if ($success > 0) {
-		error_log("User '$username' created.");
-		echo '{"success":1}';
-			} else {
-		echo '{"success":0,"error_message":"Username Exist."}';
-			}
-		}
-			} else {
-		echo '{"success":0,"error_message":"Passwords does not match."}';
-		}
-			} else {
-		echo '{"success":0,"error_message":"Invalid Username."}';
-		}
-			}else {
-		echo '{"success":0,"error_message":"Invalid Data."}';
-		}
+if ($id) {
+error_log("User $username: password match.");
+echo '{"success":1}';
+} else {
+error_log("User $username: password doesn't match.");
+echo '{"success":0,"error_message":"Invalid Username/Password"}';
+}
+}
+} else {
+echo '{"success":0,"error_message":"Invalid Username/Password."}';
+}
+}else {
+echo '{"success":0,"error_message":"Invalid Data."}';
+}
 ?>
